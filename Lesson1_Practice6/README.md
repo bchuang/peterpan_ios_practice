92a2320520e6a4de5f9e34cf0bb5bcefd7eca81e
Chuang Boris <boris855161@gmail.com>
2017-10-28 18:04:44 +0800

    Create Tabbed APP (add .MD to record Tabbed APP information)

---
+++ Lesson1_Practice6/README.md
@@ -0,0 +1,12 @@
+# peterpan_ios_practice
+Create a Tabbed Project
+
+Default "Main.storyboard" include 3 Scene and 2 Segue
+    [Scene]
+    1.Tab Bar Controller
+    2.First View Controller
+    3.Second View Controller
+    [Segue]
+    1.Relationship Segue  "view controllers" to "First"
+    2.Relationship Segue  "view controllers" to "Second"
+
---
+++ Lesson1_Practice6/Tabbed APP/Tabbed APP/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tabbed APP
+//
+//  Created by Chuang Boris on 28/10/2017.
+//  Copyright © 2017 Bchuang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
