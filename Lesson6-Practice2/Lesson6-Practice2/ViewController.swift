91e5d3392319ce397198cbcc1abac8edbf7efa82
Chuang Boris <boris855161@gmail.com>
2017-11-12 12:50:26 +0800

    Add Lesson6-Practice2(Guess Number). draw View & Layout

---
+++ Lesson6-Practice2/Lesson6-Practice2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson6-Practice2
+//
+//  Created by Chuang Boris on 12/11/2017.
+//  Copyright © 2017 Chuang Boris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
